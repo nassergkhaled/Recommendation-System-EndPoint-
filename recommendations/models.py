e4f64affec570fbbe2f7998b7bc9951d83f3dd7c
Nasser Al-den Ghassan Shareef Khaled <nassergk1@gmail.com>
2024-06-25 23:38:17 +0300

    Update project with signals and other changes

--- recommendations/models.py
+++ recommendations/models.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.db import models
 
-
 class User(models.Model):
     username = models.CharField(max_length=100)
     email = models.EmailField()
-    preferences = models.JSONField(default=dict)  # Store user preferences as JSON
+    preferences = models.JSONField(default=dict)
+    alt_id = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.username
@@ -15,6 +15,7 @@ class Apartment(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     recommended_score = models.FloatField()
+    alt_id = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -22,7 +23,8 @@ class Apartment(models.Model):
 class UserInteraction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
-    rating = models.IntegerField()  # Or other interaction metric
+    rating = models.IntegerField()
+    alt_id = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.apartment.title} - {self.rating}"
