8a812cc47217f568052c305b458cc33e9d191f17
Nasser Al-den Ghassan Shareef Khaled <nassergk1@gmail.com>
2024-06-26 10:25:24 +0300

    Update project with signals and other changes

--- recommendations/admin.py
+++ recommendations/admin.py
@@ -3,18 +3,18 @@ from .models import User, Apartment, UserInteraction
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email')  # Display user IDs along with username and email
+    list_display = ('id', 'username', 'email','alt_id')  # Display user IDs along with username and email
     search_fields = ('username', 'email')
 
 @admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'recommended_score')
+    list_display = ('title', 'price', 'recommended_score','alt_id')
     search_fields = ('title',)
     list_filter = ('price', 'recommended_score')
 
 @admin.register(UserInteraction)
 class UserInteractionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'apartment', 'rating')
+    list_display = ('user', 'apartment', 'rating','alt_id')
     search_fields = ('user__username', 'apartment__title')
     list_filter = ('rating',)
 
